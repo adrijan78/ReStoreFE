72ee47ba7c9eb0c755e3dfd39cbd93e9441b8ed8
Adrijan173099 <ado-chelsea@live.com>
2023-11-09 15:18:30 +0100

    adding checkout funct. and adding stripe

---
+++ src/app/components/UI/AppCheckbox.tsx
@@ -0,0 +1,31 @@
+
+
+import { FormControlLabel,Checkbox } from '@mui/material';
+import { UseControllerProps } from 'react-hook-form';
+import { useController } from 'react-hook-form';
+
+interface Props extends UseControllerProps{
+    label:string;
+    disabled:boolean
+}
+
+const AppCheckbox = (props:Props) => {
+    const {field} = useController({...props, defaultValue:false})
+  return (
+   <FormControlLabel 
+     control={
+        <Checkbox
+        {...field}
+        checked={field.value}
+        color='secondary'
+        disabled ={props.disabled}
+        />
+     }
+     label={props.label}
+   /> 
+        
+    
+  )
+}
+
+export default AppCheckbox
--- src/app/models/basket.ts
+++ src/app/models/basket.ts
@@ -2,6 +2,8 @@ export interface Basket {
   id: number;
   buyerId: string;
   items: Item[];
+  paymentIntentId?: string;
+  clientSecret?: string;
 }
 
 export interface Item {
